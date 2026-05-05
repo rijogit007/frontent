d39d0a516001866d3fd11d8f19b30318e1bd2013
Rijo007 <rijovarghese225@gmail.com>
2026-05-05 21:53:14 +0530

    newcommit

---
+++ js/function/function.js
@@ -0,0 +1,30 @@
+// function myFunc(){
+//     let a=5;
+//     myFunctwo();
+// console.log(a)
+
+// }
+
+
+
+
+
+// function myFunctwo(){
+//     let a=10;
+//     console.log("completed")
+
+
+// }
+
+// myFunc();
+
+// arrow function
+
+
+Myfunc= (c) =>{
+
+return c;
+
+}
+
+console.log(Myfunc(10))
