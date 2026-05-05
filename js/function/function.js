73f9f40685d1a873cdaaf0a5169437573105d6ee
Rijo007 <rijovarghese225@gmail.com>
2026-05-05 22:40:29 +0530

    newcommit

--- js/function/function.js
+++ js/function/function.js
@@ -21,10 +21,59 @@
 // arrow function
 
 
-Myfunc= (c) =>{
+// Myfunc= (c) =>{
 
-return c;
+// return c;
 
-}
+// }
 
-console.log(Myfunc(10))
+// console.log(Myfunc(10))
+
+
+// objects
+
+
+// let user ={
+
+//     firstname:"rijo",
+//     lastname:"varghese"
+// }
+
+// console.log(user.firstname)
+
+
+//array
+
+
+// let myarray=["hello",10,"text"]
+
+// console.log(myarray);
+// console.log(myarray[0]);
+
+
+//methods
+
+
+// let a="rijoi";
+
+// // console.log(a.indexOf("i"));
+// console.log(a.lastIndexOf("i"));
+
+
+// let a="rijo varghese";
+
+// let b=a.slice(0,3);
+
+// console.log(b)
+
+
+let  a="15";
+let b=10;
+
+
+console.log(typeof(a));
+console.log(typeof(b));
+
+
+console.log(typeof parseInt (a));
+console.log(typeof b.toString() );
