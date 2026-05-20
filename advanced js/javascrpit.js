b345a669d2bc6d7ee65d2b4b8d6298bf6f6c830c
Rijo007 <rijovarghese225@gmail.com>
2026-05-21 02:29:49 +0530

    new commit

--- advanced js/javascrpit.js
+++ advanced js/javascrpit.js
@@ -1,21 +1,53 @@
-//spread opertor
+// //spread opertor
 
-const globalplayer={
+// // const globalplayer={
 
-    id:0,
+// //     id:0,
+// //     name:"rijo",
+// //     power:100,
+// // };
+
+
+// // const  globalplayer2={...globalplayer};//pass copy
+
+
+
+// // globalplayer2.name="rijo22";
+
+
+// // console.log(globalplayer)
+// // console.log(globalplayer2)
+
+
+// const player=["Ramu","Ajay","john"];
+
+
+// const player2={...player}
+
+// player2.powerlevr=50;
+
+// console.log(player)
+// console.log(player2)
+
+
+const globalplayer=[
+
+{
+    
+    id:1,
     name:"rijo",
     power:100,
-};
+}
+
+]
+
+const details=globalplayer.map((data)=> data.id);
+console.log(details)
 
 
-const  globalplayer2={...globalplayer};//pass copy
 
+// const number=[15,10,20,34,21,3,1,5,15];
 
+// const sortnumber=number.sort((a,b)=> a-b);
 
-globalplayer2.name="rijo22";
-
-
-console.log(globalplayer)
-console.log(globalplayer2)
-
-
+// console.log(number)
