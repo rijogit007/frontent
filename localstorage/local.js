66f29ede5071cceb7211d85130bae8d0fe9a944d
Rijo007 <rijovarghese225@gmail.com>
2026-06-21 01:38:05 +0530

    new commit

--- localstorage/local.js
+++ localstorage/local.js
@@ -5,7 +5,7 @@
 // localStorage.key();
 
 
-localStorage.setItem("id","0");
+// localStorage.setItem("id","0");
 // localStorage.setItem("token","abcdef")
 // localStorage.setItem("name","rijo")
 
@@ -21,13 +21,40 @@ localStorage.setItem("id","0");
 // document.write(localStorage.getItem("token"))
 
 
-const obj={
+// const obj={
 
-    id:0,
-    token:"abcdef",
+//     id:0,
+//     token:"abcdef",
 
-    name:"rijo"
-};
+//     name:"rijo"
+// };
 
-localStorage.setItem("data",JSON.stringify(obj))
+// localStorage.setItem("data",JSON.stringify(obj))
+
+// console.log(JSON.parse(localStorage.getItem("data")))
+
+const input=document.querySelector("input");
+const login=document.querySelector("#login");
+const logout=document.querySelector("#logout");
+const h1=document.querySelector("h1");
+if (localStorage.getItem("token")){
+
+    h1.innerHTML="welcome user";
+}
+login.onclick=()=>{
+    if(input.value){
+
+        localStorage.setItem("token",input.value);
+        h1.innerText="welcome user";
+
+        
+    }
+
+}
+logout.onclick=()=>{
+localStorage.clear();
+locatiom.reload();
+
+}
+
 
